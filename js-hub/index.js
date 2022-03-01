c9add0131d4c1575b1646b6d498bd2ddd04e2b6c
Ahsan Habib <ahsanpub16b@gmail.com>
2022-03-01 10:14:16 +0600

    getting data from api and clear the search bar here

--- js-hub/index.js
+++ js-hub/index.js
@@ -3,6 +3,22 @@
 const searchPhone = () => {
     const searchBar = document.getElementById('search-field');
     // console.log(searchBar);
-    const textSearch = searchBar.value ;
-    console.log(textSearch);
+    const textSearch = searchBar.value;
+    // console.log(textSearch);
+
+
+     //clear data 
+     searchBar.value = '';
+
+
+    // data Loading dynamically Api here 
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${textSearch}`
+// console.log(url);
+  
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data));
+
 }
