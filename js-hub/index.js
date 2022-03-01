771076433005b2c2bd09ca12b4adca7b06fc4569
Ahsan Habib <ahsanpub16b@gmail.com>
2022-03-01 11:30:51 +0600

    display the search result works here

--- js-hub/index.js
+++ js-hub/index.js
@@ -19,6 +19,38 @@ const searchPhone = () => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displaySearchResult(data.data));
 
+}
+
+
+// display Result Function here 
+
+const displaySearchResult = phones => {
+    // console.log(phones);
+    const searchResult = document.getElementById('search-result');
+
+    searchResult.textContent = '';
+
+    phones.forEach(data => {
+        // console.log(data);
+        const div = document.createElement('div');
+        div.classList.add('col');
+
+        div.innerHTML= `
+        
+        <div class="card">
+        <h4 class="card-title text-center">${data.brand}</h4>
+        <img src="${data.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${data.phone_name}</h5>
+          <button type="button" class="btn btn-success">Show Detail</button>
+        </div>
+      </div>
+        
+        
+        `
+        searchResult.appendChild(div);
+
+    });
 }
