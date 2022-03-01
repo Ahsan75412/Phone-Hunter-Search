d9847c6a14d02de2fb032baa535e2a5e5779d14d
Ahsan Habib <ahsanpub16b@gmail.com>
2022-03-01 09:55:23 +0600

    search bar value shows the console in text

--- js-hub/index.js
+++ js-hub/index.js
@@ -1,1 +1,8 @@
-console.log('Linked !');
+
+
+const searchPhone = () => {
+    const searchBar = document.getElementById('search-field');
+    // console.log(searchBar);
+    const textSearch = searchBar.value ;
+    console.log(textSearch);
+}
